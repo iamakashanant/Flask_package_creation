f05f757c395e6965d70624dd2ceace38a995f45f
Akash Anant <akash@magentaconnect.com>
2020-01-04 18:39:20 +0530

    Final_user_profile_update

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -66,17 +66,11 @@ def about_page():
 
 
 def save_picture(form_picture):
-    # random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
-    # picture_fn = random_hex + f_ext
     user_name=User.query.filter_by(username=current_user.username).first()	
     picture_fn= str(user_name.id) + "_" + _ +"_" + str(datetime.now().timestamp()).replace('.','') + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
     form_picture.save(picture_path)
-    # output_size = (125, 125)
-    # i = Image.open(form_picture)
-    # i.thumbnail(output_size)
-    # i.save(picture_path)
     return picture_fn
 
 
